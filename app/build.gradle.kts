47480f57817ed76bab613129a82f0144d10a6087
ChinmayGawad <chinmaygawad86@gmail.com>
2024-04-02 10:48:28 +0530

    styles.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.android.gms:play-services-nearby:19.1.0")
+    implementation("com.android.car.ui:car-ui-lib:2.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
